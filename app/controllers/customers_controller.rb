30b1f639d68c6287e15718f1c73a5277c7eca1ad
nowallcloud9_003 <nowallcloud9+003@gmail.com>
2017-07-30 07:37:11 +0000

    コメント登録機能追加

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,68 @@
+class CustomersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+  def index
+    @q = Customer.ransack(params[:q])
+    @customers = @q.result.page(params[:page])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if  @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
+  end
+
+  def edit
+   @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if  @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @customer.comments
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to customers_path
+  end
+  
+  
+private
+ 
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email_id,
+      :company_id,
+      :post_id)
+  end  
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+end
+
+
+
+
+
+
+
+ 
